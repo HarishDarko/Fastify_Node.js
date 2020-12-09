dd7d6745271e53b9ff0d015ba90c1d0fa58507be
HarishDarko <harishdarko@gmail.com>
2020-12-09 19:34:40 +0530

    json implementation

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const fastify = require('fastify')({ logger: true })
 const path = require('path')
+const fs = require('fs');
+
+let student={}
+fs.readFile('data.json', (err, data) => {
+  if (err) throw err;
+  student = JSON.parse(data);
+  console.log(student);
+});
+
+
+fastify.register(require('point-of-view'), {
+  engine: {
+    ejs: require('ejs')
+  }
+})
 
 fastify.register(require('fastify-static'), {
     root: path.join(__dirname, 'view'),
@@ -7,7 +22,9 @@ fastify.register(require('fastify-static'), {
   })
 
 fastify.get('/', function (req, reply) {
-    return reply.sendFile('index.html') // serving path.join(__dirname, 'public', 'myHtml.html') directly
+    reply.view('index.ejs',{data:student}) 
+    
+    // serving path.join(__dirname, 'public', 'myHtml.html') directly
   })
   
 // fastify.get('/path/with/different/root', function (req, reply) {
